585a0e2768a10f7911f062d4b2e02f2d02ab531c
Magic-Kowel <58827128+Magic-Kowel@users.noreply.github.com>
2021-06-03 03:00:13 -0500

    implementando FlatList

--- App.js
+++ App.js
@@ -1,18 +1,37 @@
 import React, {useState} from 'react';
 
-// definir el state de sitas
+
 import {
   Text,
   StyleSheet,
-  View
+  View,
+  FlatList
 } from 'react-native';
 
 const App= () => {
+  // definir el state de citas
+const [citas, setCitas] = useState([
+  {id: "1", paciente: "Hook", propietario:'juan', sintomas: 'no come'},
+  {id: "2", paciente: "Redux", propietario:'kowel', sintomas: 'no canta'},
+  {id: "3", paciente: "Native", propietario:'Josue', sintomas: 'no baila'}
+]);
   return (
     <View style={styles.contenedor}>
       <Text style={styles.titulo}>
       Prollecto citas
       </Text>
+      <FlatList 
+        data={citas}
+        renderItem={ ({item}) =>(
+          <View>
+            <Text>{
+              item.paciente
+            }</Text>
+          </View>
+        )
+        }
+        keyExtractor={cita => cita.id}
+      />
     </View>
   );
 };
