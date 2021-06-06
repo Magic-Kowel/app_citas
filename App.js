639352fb0f3f1339ca058185e19a1e3d59c310b3
Magic-Kowel <58827128+Magic-Kowel@users.noreply.github.com>
2021-06-06 15:54:24 -0500

    formolario terminado

--- App.js
+++ App.js
@@ -1,43 +1,120 @@
-import React, {useState} from 'react';
-import {
-  Text,
-  StyleSheet,
-  View,
-  FlatList
-} from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { Text, StyleSheet,
+    View,
+    FlatList,
+    TouchableHighlight,
+    TouchableWithoutFeedback,
+    Keyboard,
+    Platform  } from 'react-native';
 import Cita from './componentes/Cita';
-const App= () => {
+import Formulario from './componentes/Formulario';
+
+const App = () => {
   // definir el state de citas
-const [citas, setCitas] = useState([
-  {id: "1", paciente: "Hook", propietario:'juan', sintomas: 'no come'},
-  {id: "2", paciente: "Redux", propietario:'kowel', sintomas: 'no canta'},
-  {id: "3", paciente: "Native", propietario:'Josue', sintomas: 'no baila'}
-]);
+  const [citas, setCitas] = useState([]);
+  const [mostrarform, guardarMostrarForm] = useState(false);
+
+
+
+
+  // Elimina los pacientes del state
+  const eliminarPaciente = id => {
+
+    const citasFiltradas = citas.filter( cita => cita.id !== id );
+    setCitas( citasFiltradas );
+    guardarCitasStorage(JSON.stringify(citasFiltradas));
+  }
+
+  // Muestra u oculta el Formulario
+  const mostrarFormulario = () => {
+    guardarMostrarForm(!mostrarform);
+  }
+
+  // Ocultar el teclado
+  const cerrarTeclado = () => {
+    Keyboard.dismiss();
+  }
+
+  // Almacenar las citas en storage
+  const guardarCitasStorage = async (citasJSON) => {
+    try {
+      await AsyncStorage.setItem('citas', citasJSON);
+    } catch (error) {
+        console.log(error);
+    }
+  }
+
   return (
-    <View style={styles.contenedor}>
-      <Text style={styles.titulo}>
-      Prollecto citas
-      </Text>
-      <FlatList 
-        data={citas}
-        renderItem={ ({item}) =><Cita  item={item} />
-        }
-        keyExtractor={cita => cita.id}
-      />
-    </View>
+    <TouchableWithoutFeedback onPress={() => cerrarTeclado() }>
+      <View style={styles.contenedor}>
+          <Text style={styles.titulo}>Administrador de Citas</Text>
+
+          <View>
+              <TouchableHighlight onPress={ () => mostrarFormulario() } style={styles.btnMostrarForm}>
+                  <Text style={styles.textoMostrarForm}> {mostrarform ? 'Cancelar Crear Cita' : 'Crear Nueva Cita'} </Text>
+              </TouchableHighlight>
+          </View>
+
+          <View style={styles.contenido}>
+            { mostrarform ? (
+              <>
+                <Text style={styles.titulo}>Crear Nueva Cita</Text>
+                <Formulario 
+                  citas={citas}
+                  setCitas={setCitas}
+                  guardarMostrarForm={guardarMostrarForm}
+                  guardarCitasStorage={guardarCitasStorage}
+                />
+              </>
+            ) : (
+              <>
+                <Text style={styles.titulo}> {citas.length > 0 ? 'Administra tus citas' : 'No hay citas, agrega una'} </Text>
+                <FlatList 
+                    style={styles.listado}
+                    data={citas}
+                    renderItem={ ({item}) => <Cita item={item} eliminarPaciente={eliminarPaciente} />  }
+                    keyExtractor={ cita => cita.id}
+                />
+              </>
+            ) }
+            
+          
+          </View>
+
+      </View>
+    </TouchableWithoutFeedback>
   );
 };
-const styles =StyleSheet.create({
-  contenedor:{
+
+const styles = StyleSheet.create({
+  contenedor: {
     backgroundColor: '#AA076B',
-    flex:1
+    flex: 1
   },
-  titulo:{
-    color:'#FFF',
-    marginTop: 40,
-    fontSize:24,
+  titulo: {
+    color: '#FFF',
+    marginTop: Platform.OS === 'ios' ?  40  : 20 ,
+    marginBottom: 20,
+    fontSize: 24,
     fontWeight: 'bold',
     textAlign: 'center'
+  }, 
+  contenido: {
+    flex: 1,
+    marginHorizontal: '2.5%',
+  },
+  listado: {
+    flex: 1,
+  },
+  btnMostrarForm: {
+      padding: 10,
+      backgroundColor:'#7d024e',
+      marginVertical: 10
+  },
+  textoMostrarForm: {
+      color: '#FFF',
+      fontWeight: 'bold',
+      textAlign: 'center'
   }
 });
 
